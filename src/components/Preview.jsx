1cd0bcd1f0057b3d0c74dae4a65723b6f74fbf44
hbc <bcxxxxxx@gmail.com>
2018-07-11 16:17:57 +0800

    add proof-of-work demo

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { StyleRoot } from 'radium';
+import GIFEncoder from 'gif-encoder';
+import blobStream from 'blob-stream';
 import {
   generatePixelDrawCss,
   generateAnimationCSSData
@@ -51,6 +53,73 @@ const Preview = (props) => {
     );
   };
 
+  const generateCanvasPreview = () => {
+    const canvasStyle = {
+      position: 'fixed',
+      left: 40,
+    };
+
+    const {
+      frames, columns, rows, cellSize,
+    } = props.storedData || props;
+
+    const width = columns * cellSize;
+    const height = rows * cellSize;
+
+    const renderFrameToCanvas = (canvas, frame) => {
+      const ctx = canvas.getContext('2d');
+      ctx.canvas.width = width;
+      ctx.canvas.height = height;
+
+      const grids = frame.get('grid');
+      for (let i = 0, x = 0, y = 0; i < grids.size; i += 1) {
+        ctx.fillStyle = grids.get(i);
+        ctx.fillRect(x, y, cellSize, cellSize);
+
+        x += cellSize;
+        if (i % columns === columns - 1) {
+          y += cellSize;
+          x = 0;
+        }
+      }
+
+      return ctx.getImageData(0, 0, width, height).data;
+    };
+
+    const renderFrames = () => {
+      const canvas = document.createElement('canvas');
+
+      const gif = new GIFEncoder(width, height);
+      gif.pipe(blobStream())
+        .on('finish', function () {
+          const blobURL = this.toBlobURL();
+          const a = document.createElement('a');
+          a.style.display = 'none';
+          a.href = blobURL;
+          a.setAttribute('download', 'hello.gif');
+          document.body.appendChild(a);
+          a.click();
+          document.body.removeChild(a);
+          setTimeout(() => {
+            window.URL.revokeObjectURL(blobURL);
+          }, 100);
+        });
+
+      gif.writeHeader();
+      frames.forEach((frame) => {
+        gif.addFrame(renderFrameToCanvas(canvas, frame));
+        gif.setDelay(frame.get('duration'));
+      });
+      gif.finish();
+    };
+
+    return (
+      <div style={canvasStyle}>
+        <button onClick={renderFrames}>download gif</button>
+      </div>
+    );
+  };
+
   const { columns, rows, cellSize } = props.storedData || props;
   const style = {
     width: columns * cellSize,
@@ -59,6 +128,7 @@ const Preview = (props) => {
 
   return (
     <div className="preview" style={style}>
+      {generateCanvasPreview()}
       {generatePreview()}
     </div>
   );
